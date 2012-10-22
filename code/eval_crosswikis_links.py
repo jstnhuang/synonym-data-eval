20ce2a269fb30797a496116c068d8a3d6c265115
Justin Huang <jstn@cs.washington.edu>
2012-10-22 13:39:56 -0700

    Eval and print rank 1 functions.

--- code/eval_crosswikis_links.py
+++ code/eval_crosswikis_links.py
@@ -7,9 +7,9 @@
 # column. We want to see how many of these entities were correctly linked, and
 # with what probability.
 
-import re
-import sqlite3
-
+SYNONYM_DEV_SET = (
+  '/home/jstn/research/knowitall/synonym-data-eval/data/str-to-ents-dev-set'
+)
 SYNONYM_ENTITY_DIST_PATH = (
   '/home/jstn/research/knowitall/synonym-data-eval/results/cw-entity-dist.tsv'
 )
@@ -41,6 +41,26 @@ def makeLinkData(cwLinkFile):
       cwLinkData[correctEntity] = {synonym: [entityTuple]}
   return cwLinkData
 
+def getSynonymSet(synonymSetFile):
+  """Reads a set of synonyms in a file where each line is of the form:
+    {fbid}\t{entityName}\t{synonym1}\t{synonym2}\t...
+
+    Args:
+      synonymSetFile: The synonym set file.
+
+    Returns:
+      A dict mapping the entity name to its list of synonyms.
+  """
+  synonymSet = {}
+  for line in synonymSetFile:
+    lineParts = line.split('\t')
+    lineParts = [part for part in lineParts if part != '' and part != '\n']
+    lineParts = [part.strip() for part in lineParts]
+    entityName = lineParts[1]
+    synonyms = lineParts[2:]
+    synonymSet[entityName] = synonyms
+  return synonymSet
+
 def printLink(entity, synonym, cprob, rank):
   """Prints an entity link from the Crosswikis data.
 
@@ -59,7 +79,7 @@ def printLink(entity, synonym, cprob, rank):
     )
   )
 
-def printCorrectLinkData(cwLinkData):
+def printCorrectLinkData(synonymSet, cwLinkData):
   """Prints the link data for when a correct link was made.
 
   Args:
@@ -69,14 +89,91 @@ def printCorrectLinkData(cwLinkData):
   """
   for correctEntity, synonymEntities in cwLinkData.items():
     for synonym, entityList in synonymEntities.items():
-      for index, (entity, cprob, num, denom) in enumerate(entityList, start=1):
-        if entity == correctEntity:
-          printLink(entity, synonym, cprob, index)
+      if synonym in synonymSet[correctEntity]:
+        for idx, (entity, cprob, num, denom) in enumerate(entityList, start=1):
+          if entity == correctEntity:
+            printLink(entity, synonym, cprob, idx)
+
+def printRank1Links(synonymSet, cwLinkData):
+  """Prints the most likely entity given a string, as well as its likelihood.
+
+  Also prints out the ratio of the probability of the first entity to the
+  probability of the second entity.
+
+  Args:
+    synonymSet: A dict mapping entities to its list of synonyms.
+    cwLinkData: A dict with correctEntity as the key, that maps to nested dicts
+      with the synonym as the key, which maps to a list of (entity, cprob, num,
+      denom) tuples.
+  """
+  count = 0
+  for correctEntity, synonyms in synonymSet.items():
+    for synonym in synonyms:
+      count += 1
+      if synonym in cwLinkData[correctEntity]:
+        entity, cprob, num, denom = cwLinkData[correctEntity][synonym][0]
+        prob2Ratio = None
+        if len(cwLinkData[correctEntity][synonym]) > 1:
+          cprob2 = cwLinkData[correctEntity][synonym][1][1]
+          prob2Ratio = cprob/cprob2 if cprob2 != 0 else 'Inf'
+        print('{correct}\t{synonym}\t{entity}\t{cprob}\t{prob2Ratio}'.format(
+            correct=correctEntity,
+            synonym=synonym,
+            entity=entity,
+            cprob=cprob,
+            prob2Ratio=prob2Ratio
+          )
+        )
+      else:
+        print('{correct}\t{synonym}\tNone'.format(
+            correct=correctEntity,
+            synonym=synonym
+          )
+        )
+
+def evalRank1Test(synonymSet, cwLinkData):
+  """Finds the precision and recall when we just pick the most likely entity.
+
+  We pick the most likely entity in the distribution, but subject it to some
+  cutoff c. We vary c from 0 to 0.95 in the outermost loop to see the effect of
+  the cutoff on precision and recall.
+
+  Args:
+    synonymSet: A dict mapping entities to its list of synonyms.
+    cwLinkData: A dict with correctEntity as the key, that maps to nested dicts
+      with the synonym as the key, which maps to a list of (entity, cprob, num,
+      denom) tuples.
+  """
+  numSynonyms = sum([len(list) for list in synonymSet.values()])
+  print('CProb cutoff\tPrecision\tRecall')
+
+  for cprobCutoff in [x/100 for x in range(0, 100, 5)]:
+    numCorrect = 0
+    numRetrieved = 0
+
+    for correctEntity, synonymEntities in cwLinkData.items():
+      for synonym, entityList in synonymEntities.items():
+        if synonym not in synonymSet[correctEntity]:
+          continue
+        entity, cprob, num, denom = entityList[0]
+        if cprob >= cprobCutoff:
+          numRetrieved += 1
+          if entity == correctEntity:
+            numCorrect += 1
+    precision = numCorrect / numRetrieved
+    recall = numCorrect / numSynonyms
+    print('{0}\t{1}\t{2}'.format(cprobCutoff, precision, recall))
 
 def main():
   cwLinkFile = open(SYNONYM_ENTITY_DIST_PATH)
   cwLinkData = makeLinkData(cwLinkFile)
-  printCorrectLinkData(cwLinkData)
+  synonymDevSetFile = open(SYNONYM_DEV_SET)
+  synonymDevSet = getSynonymSet(synonymDevSetFile)
+
+  evalRank1Test(synonymDevSet, cwLinkData)
+  #printCorrectLinkData(synonymDevSet, cwLinkData)
+  #print(sum([len(list) for list in synonymDevSet.values()]))
+  #printRank1Links(synonymDevSet, cwLinkData)
 
 if __name__ == '__main__':
   main()
